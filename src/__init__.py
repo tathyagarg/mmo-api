900c75abb8f48805940fdfc135a4748777ff03ac
Tathya Garg <coding.tathya@gmail.com>
2025-02-26 15:21:20 +0530

    fix: Fixed bug that doesn't create parent `data` directory

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,8 @@ from pathlib import Path
 import json
 
 if not Path(DATABASE).exists():
+    if not Path(DATABASE).parent.exists():
+        Path(DATABASE).parent.mkdir(parents=True)
+
     with open(DATABASE, "w") as f:
         json.dump({}, f)
